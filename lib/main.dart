7b8ca665f15ee8963e689d5474a707465072cdd2
nicolaskms <130314220+nicolaskms@users.noreply.github.com>
2024-11-01 09:18:17 -0300

    Estilização lesson screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:invisto_app/screens/lesson_screen.dart';
 import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: LessonScreen(),
     );
   }
 }
