60b2bca19a67eae891697bf7310caf9de4f1f4f8
nicolaskms <130314220+nicolaskms@users.noreply.github.com>
2024-11-06 07:18:44 -0300

    Correção design registerscreen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -28,7 +27,6 @@ class _RegisterPageState extends State<RegisterPage> {
         _isLoading = true;
       });
 
-      // Dados do formulário
       Map<String, String> body = {
         'name': _name.text,
         'email': _email.text,
@@ -43,7 +41,6 @@ class _RegisterPageState extends State<RegisterPage> {
           ? 'http://localhost:5001/users/registration'
           : 'http://10.0.2.2:5001/users/registration';
 
-      // Chamada à API
       var response = await http.post(
         Uri.parse(baseUrl),
         headers: {"Content-Type": "application/json"},
@@ -52,45 +49,34 @@ class _RegisterPageState extends State<RegisterPage> {
 
       if (response.statusCode == 201) {
         var jsonResponse = jsonDecode(response.body);
-
-        // Se o cadastro for bem-sucedido, autenticar usuário
         _authenticateUser(_email.text, _password.text);
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text('Conta criada com sucesso!'),
         ));
         Navigator.pop(context);
 
-        //Com o cadastro bem-sucedido, chamar a função da API que adiciona o UID no banco de dados
+        var userUid = FirebaseAuth.instance.currentUser?.uid;
+        var userEmail = FirebaseAuth.instance.currentUser?.email;
+        Map<String, String?> bodyRequest = {
+          'uid': userUid,
+          'email': userEmail
+        };
 
-          //Pegar o uid do usuário logado
-          var userUid = FirebaseAuth.instance.currentUser?.uid;
-          var userEmail = FirebaseAuth.instance.currentUser?.email;
-          //Criando body do request
-          Map<String, String?> bodyRequest = {
-            'uid':userUid,
-            'email':userEmail
-          };
+        var responseUid = await http.post(
+          Uri.parse('http://localhost:5001/users/addUidUser'),
+          headers: {"Content-Type": "application/json"},
+          body: jsonEncode(bodyRequest),
+        );
 
-          // Chamada à API
-          var responseUid = await http.post(
-            Uri.parse('http://localhost:5001/users/addUidUser'),
-            headers: {"Content-Type": "application/json"},
-            body: jsonEncode(bodyRequest),
-          );
-
-          if(responseUid.statusCode == 201){
-            // Sucesso
-          }else{
-            // Tratar erros
-            var jsonResponse = jsonDecode(responseUid.body);
-            var message = jsonResponse['error'] ?? 'Erro, entre em contato com o suporte!';
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text(message),
-            ));
-          }
+        if (responseUid.statusCode != 201) {
+          var jsonResponse = jsonDecode(responseUid.body);
+          var message = jsonResponse['error'] ?? 'Erro, entre em contato com o suporte!';
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(message),
+          ));
+        }
 
       } else {
-        // Tratar erros
         var jsonResponse = jsonDecode(response.body);
         var message = jsonResponse['error'] ?? 'Erro, entre em contato com o suporte!';
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -121,15 +107,14 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SingleChildScrollView( // Permite rolar a tela
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            // Botão de Voltar no canto superior esquerdo
             Align(
               alignment: Alignment.topLeft,
               child: Padding(
-                padding: const EdgeInsets.only(top: 40.0), // Aumentado para 40
+                padding: const EdgeInsets.only(top: 25.0),
                 child: TextButton(
                   onPressed: () {
                     Navigator.pop(context);
@@ -144,17 +129,13 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               ),
             ),
-
-            // Logo e título
-            const SizedBox(height: 10),
+            const SizedBox(height: 5),
             Image.asset(
               'assets/images/BlackSemFrase.png',
-              height: 150, // Altura do logo
+              height: 150,
               width: 200,
             ),
             const SizedBox(height: 10),
-
-            // Container roxo com gradiente e formulário
             Container(
               width: double.infinity,
               decoration: const BoxDecoration(
@@ -170,49 +151,45 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                // Formulário de registro
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 15),
-                      _buildTextField(_name, 'Nome Completo'),
-                      const SizedBox(height: 20),
-                      _buildTextField(_email, 'Email'),
-                      const SizedBox(height: 20),
-                      _buildPasswordField(_password, 'Senha'),
-                      const SizedBox(height: 20),
-                      _buildPasswordField(_confirmPassword, 'Confirmação de Senha'),
-                      const SizedBox(height: 20),
-                      _buildTextField(_cpf, 'CPF'),
-                      const SizedBox(height: 20),
-                      _buildTextField(_dob, 'Data de nascimento'),
-                      const SizedBox(height: 20),
-                      _buildTextField(_phone, 'Telefone'),
-                      const SizedBox(height: 30),
-                      _isLoading
-                          ? const CircularProgressIndicator()
-                          : ElevatedButton(
-                        onPressed: _registerUser,
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 100, vertical: 15),
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: const Text(
-                          'Criar conta',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 40),
+                    _buildTextField(_name, 'Nome Completo'),
+                    const SizedBox(height: 20),
+                    _buildTextField(_email, 'Email'),
+                    const SizedBox(height: 20),
+                    _buildPasswordField(_password, 'Senha'),
+                    const SizedBox(height: 20),
+                    _buildPasswordField(_confirmPassword, 'Confirmação de Senha'),
+                    const SizedBox(height: 20),
+                    _buildTextField(_cpf, 'CPF'),
+                    const SizedBox(height: 20),
+                    _buildTextField(_dob, 'Data de nascimento'),
+                    const SizedBox(height: 20),
+                    _buildTextField(_phone, 'Telefone'),
+                    const SizedBox(height: 30),
+                    _isLoading
+                        ? const CircularProgressIndicator()
+                        : ElevatedButton(
+                      onPressed: _registerUser,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 100, vertical: 15),
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
                         ),
                       ),
-                    ],
-                  ),
+                      child: const Text(
+                        'Criar conta',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
                 ),
               ),
             ),
@@ -222,7 +199,6 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  // Função para criar campos de texto
   Widget _buildTextField(TextEditingController controller, String labelText) {
     return TextFormField(
       controller: controller,
@@ -244,9 +220,7 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  // Função para criar campos de senha
-  Widget _buildPasswordField(
-      TextEditingController controller, String labelText) {
+  Widget _buildPasswordField(TextEditingController controller, String labelText) {
     return TextFormField(
       controller: controller,
       obscureText: true,
